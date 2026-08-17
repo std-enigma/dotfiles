37690c8287488004f0070e700cec300be5c0e60d
Ilia Bahrebar <std.ilia.bahrebar@gmail.com>
2026-08-17 04:48:18 +0330

    feat(hypr): add decorations module

---
+++ private_dot_config/hypr/decorations.lua
@@ -0,0 +1,22 @@
+hl.config({
+	general = {
+		gaps_in = 3,
+		gaps_out = 8,
+		border_size = 2,
+		resize_on_border = true,
+	},
+	decoration = {
+		blur = {
+			enabled = true,
+			ignore_opacity = true,
+			new_optimizations = true,
+			passes = 3,
+			size = 6,
+			xray = false,
+		},
+		rounding = 10,
+		shadow = {
+			enabled = false,
+		},
+	},
+})
